7d1d50ad56e2641723eba6ae5216d67813a6a85d
Abdallah Hodieb <abdallah.hodieb@gmail.com>
2022-11-01 11:33:07 +0000

    ♻️ refactor rebase-all to allow for more subcommands

--- cmd/rebase-all.go
+++ cmd/rebase-all.go
@@ -1,36 +1,13 @@
-package main
+package cmd
 
 import (
 	"log"
-	"os"
 
 	"github.com/ahodieb/rebaser/git"
 	"github.com/urfave/cli/v2"
 )
 
-const FlagWorkingDir = "working-dir"
-
-func main() {
-	app := &cli.App{
-		Name:   "rebaser",
-		Usage:  "rebase multiple branches onto main or master",
-		Action: RebaseAllBranches,
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        FlagWorkingDir,
-				Aliases:     []string{"C"},
-				Usage:       "set working directory `PATH`",
-				DefaultText: ".",
-			},
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func RebaseAllBranches(cCtx *cli.Context) error {
+func RebaseAll(cCtx *cli.Context) error {
 	g := git.Git{WorkingDir: cCtx.String(FlagWorkingDir)}
 
 	mainBranch, err := g.FindMainBranch()
